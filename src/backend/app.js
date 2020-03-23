72b8b6e1d78e82b4b2a701c1e565a9203b65a325
Daniel Arango <danielarango@daniels-air.lan>
2020-03-23 14:27:40 +0000

    issue 23 completed

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const app = express();
-const port = 3001;
+const path = require('path');
+const port = process.env.PORT || 3001;
 var getFilmData = require('./GalleryDataBE');
 
 
 app.get('/rest/films', (req, res) => res.send(getFilmData()));
+	
+app.use(express.static(path.join(__dirname, '../../build'))); // This serves the static files from the app´s build folder
+
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, '../../build', 'index.html'))); // React routing for our entry point
 
 app.listen(port, () => console.log(`Listening on port ${port}!`));
-
-
